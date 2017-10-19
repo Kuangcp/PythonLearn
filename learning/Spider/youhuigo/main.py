be3584f42ed81f13f9a5d83922c3750d8ff6759d
mythos <kuang.myth@foxmail.com>
2017-10-19 23:35:20 +0800

    python真的爬虫更擅长么

---
+++ learning/Spider/youhuigo/main.py
@@ -0,0 +1,17 @@
+"""
+    爬取指定的URL上的页面，分析元素，返回对应的值
+    怎么处理cookie的获取
+"""
+def read_html(url):
+    ''' 阅读指定的URL得到一个HTML的结果集'''
+    pass
+    
+def check_effective():
+    ''' 业务检测 '''
+    pass
+
+def main():
+    print("main")
+    pass
+
+main()
