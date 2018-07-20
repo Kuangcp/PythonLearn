c872bb4246ec57ac50713c7c434a39314964aa52
kuangcp <kuangcp@aliyun.com>
2018-07-20 19:42:11 +0800

    optimize some url

--- web/flask/web_redis/app/views.py
+++ web/flask/web_redis/app/views.py
@@ -2,4 +2,4 @@ from app import app
 
 @app.route('/')
 def index():
-    return '<a href="http://127.0.0.1:22334/static/index.html"><button>index</button></a>'
+    return '<a href="/static/index.html"><button>index</button></a>'
