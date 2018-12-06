313bdfcc84c64fd1097b3c36012e8a6f72abef7e
kuangcp <kuangcp@aliyun.com>
2018-12-06 14:13:51 +0800

    simplify box sort

--- learning/base/dict/first.py
+++ learning/base/dict/first.py
@@ -6,15 +6,14 @@ alien = {'color': 'green', 'age': '23', 'hobi': ['d', 'f']}
 print(alien)
 
 
-def hi(name, *names, age=0,**s):
+def read_param(name, *names, age=0,**s):
     print(name, age)
     for n in names:
         print(n)
     for ss, d in s.items():
         print(ss, d)
 
-
-hi('d', 23, 34, 45, d=6, sdsd=89)
+read_param('d', 23, 34, 45, d=6, sdsd=89)
 files = 'test.json'
 with open(files, 'w') as o:
     json.dump(alien, o)
--- learning/math/algorithm/recursion/factorial.py
+++ learning/math/algorithm/recursion/factorial.py
@@ -1,5 +1,5 @@
 
-# 递归和尾递归的对比
+# 递归和尾递归的对比, 无论是何种方式都会被Python的递归深度限制
 
 def simple(n):
     ''' 最简单的递归方式来计算阶乘'''
@@ -9,13 +9,13 @@ def simple(n):
         return n * simple(n-1)
 
 def factorial(n, result=1):
-    ''' 尾递归, 将结果带入到下一次方法调用'''
+    ''' 尾递归, 直接返回函数'''
     if n == 1:
-        return result;
+        return result
     else:
         return factorial(n-1, n*result)
 
-num = 10
+num = 1000
 print(factorial(num))
 print(simple(num))
 
